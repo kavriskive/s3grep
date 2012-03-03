5a1872b9605cf8db09cbc1df8a3dc97ae670f0ce
Barnaby Gray <barnaby@pickle.me.uk>
2012-03-03 22:18:25 +0000

    License and admin

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
+
 __version__ = '0.1'
 
 setup(name='s3grep',
       version=__version__,
-      description='grep for Amazon S3',
+      description='Parallelized grep for Amazon S3',
+      license='MIT',
       author='Barnaby Gray',
       author_email='barnaby@pickle.me.uk',
       url='http://github.com/barnybug/s3grep/',
-      download_url='http://github.com/barnybug/s3grep/tarball/'+__version__+'#egg=s3grep-'+__version__,
       requires=['boto'],
       scripts=['s3grep'],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        ],
       )
